63c06380c7424d9a209d77b771ed5f02f95be1d4
nikhil <nikhilmahato3337@gmail.com>
2024-06-10 20:44:05 +0530

    active navlinks

---
+++ src/components/NavLinks.jsx
@@ -0,0 +1,42 @@
+import { PiFlyingSaucer } from "react-icons/pi";
+import { NavLink, useLocation } from "react-router-dom";
+
+const links = [
+  { id: 2, url: "/about", text: "about" },
+  { id: 3, url: "/destinations", text: "destinations" },
+  { id: 4, url: "/services", text: "services" },
+  { id: 5, url: "/blog", text: "blog" },
+  { id: 6, url: "/gallery", text: "gallery" },
+  { id: 7, url: "/contact", text: "contact us" },
+];
+
+const NavLinks = () => {
+  const location = useLocation();
+  return (
+    <>
+      {links.map((link) => {
+        const { id, url, text } = link;
+        const isActive = location.pathname === url;
+        return (
+          <li key={id} className="">
+            <NavLink
+              className={`nav-link capitalize font-poppins px-3 ${
+                isActive ? "active" : ""
+              }`}
+              to={url}
+            >
+              {text}
+              {isActive && (
+                
+                  <PiFlyingSaucer className="text-stone-500 sm:hidden lg:block " />
+               
+              )}
+            </NavLink>
+          </li>
+        );
+      })}
+    </>
+  );
+};
+
+export default NavLinks;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
+import NavLinks from "./NavLinks";
 
 const Navbar = () => {
   return (
-    <nav className="bg-baseGrey">
-      <div className="navbar align-elememt">
+    <nav className="bg-baseGrey  ">
+      <div className="navbar align-element ">
         <div className="navbar-start">
           <NavLink to="/" className="hidden lg:flex  text-3xl items-center ">
             Logo
@@ -15,19 +16,25 @@ const Navbar = () => {
             <label tabIndex={0} className="btn btn-ghost  lg:hidden">
               <FaBarsStaggered className="h-6 w-6" />
             </label>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200">
-                nav links
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
+            >
+              <NavLinks />
             </ul>
           </div>
         </div>
         <div className="navbar-end hidden lg:flex">
-          <ul className="menu menu-horizontal"> nav links</ul>
+          <ul className="flex gap-6 text-sm font-normal tracking-wide  ">
+            {" "}
+            <NavLinks />
+          </ul>
         </div>
-        <div className="navbar-end">
+        {/* <div className="navbar-end">
           <NavLink to="/" className="flex lg:hidden  text-3xl items-center ">
             Logo
           </NavLink>
-        </div>
+        </div> */}
       </div>
     </nav>
   );
